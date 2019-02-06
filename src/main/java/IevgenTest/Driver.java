b3c79aa48cea336d2f373fdc31f151d520d4e377
levgren <jekaobodianskyi@gmail.com>
2019-02-06 21:29:54 +0200

    singletone done

--- src/main/java/IevgenTest/Driver.java
+++ src/main/java/IevgenTest/Driver.java
@@ -1,15 +1,32 @@
 package IevgenTest;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
     private static Driver ourInstance = new Driver();
 
-    public static Driver getInstance() {
-        return ourInstance;
-    }
+    public static WebDriver driver;
 
     private Driver() {
-        Driver driver = new ChromeDriver("")
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        String url = "https://dom.ria.com/ru/search/";
+        driver.get(url);
+        System.out.println("Page opens!");
+
+
+    }
+
+    public static WebDriver get() {
+        if(driver==null)
+            new Driver();
+        return driver;
     }
 }
